58fbf9a7fc8586e20a4f2d4a4a75e618ce0a3caa
Diego Fernandez Aceves <dfera10@gmail.com>
2025-10-02 10:40:20 +0200

    feat: create order repository

---
+++ src/main/java/com/fakecap/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.fakecap.repository;
+
+import com.fakecap.model.Order;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class OrderRepository implements PanacheRepository<Order> {
+
+}
