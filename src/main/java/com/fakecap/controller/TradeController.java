35f01a9b226f4a19e86f9c8ee1ff3ff24c701087
Diego Fernandez Aceves <dfera10@gmail.com>
2025-10-02 12:34:00 +0200

    feat: create trade service

--- src/main/java/com/fakecap/controller/TradeController.java
+++ src/main/java/com/fakecap/controller/TradeController.java
@@ -3,15 +3,25 @@ package com.fakecap.controller;
 import com.fakecap.ShareRequest;
 import com.fakecap.ShareResponse;
 import com.fakecap.TradeOperation;
+import com.fakecap.service.OrderService;
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
 
 @GrpcService
 public class TradeController implements TradeOperation {
 
+    private final OrderService orderService;
+
+    public TradeController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @Override
+    @Blocking
     public Uni<ShareResponse> submit(ShareRequest shareRequest) {
-        return Uni.createFrom().nullItem();
+        ShareResponse shareResponse = this.orderService.createOrder(shareRequest);
+        return Uni.createFrom().item(shareResponse);
     }
 
 }
